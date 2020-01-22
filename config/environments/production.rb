651fc1681c5f7f7a2e245fec9a7d1977ed03ee5a
Francisco Souza <francisceioseph@gmail.com>
2020-01-22 15:48:25 -0300

    fix mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "plantpedia_server_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'plantpedia-server.herokuapp.com' }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
