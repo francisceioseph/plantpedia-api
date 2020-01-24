2f346ae7fdc3bb2b54c60f967572e26eac0b4305
Francisco Souza <francisceioseph@gmail.com>
2020-01-24 16:24:21 -0300

    Add portuguese translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,29 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :authenticate_user!
+    before_action :set_locale_based_on_browser
+ 
+    def set_locale_based_on_browser
+        locale = extract_locale_from_accept_language_header
+    
+        I18n.locale =
+          if locale_valid?(locale)
+            locale
+          else
+            :en
+          end
+      end
+    
+      private
+    
+      def locale_valid?(locale)
+        I18n.available_locales.map(&:to_s).include?(locale)
+      end
+    
+      def extract_locale_from_accept_language_header
+        accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
+        return unless accept_language
+    
+        accept_language.scan(/^[a-z]{2}/).first
+      end
 end
